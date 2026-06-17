eb28efcc2f14788de37b27dc9e124eb087789cf7
yangsungbin77 <yangsungbin77@gmail.com>
2026-06-17 12:31:18 +0900

    구글 서치 콘솔 인증 메타태그 추가

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = '클릭고트래블링';
 export const SITE_DESCRIPTION = '여행지 추천부터 항공권·숙소 꿀팁까지, 떠나고 싶을 때 가장 먼저 찾는 여행 가이드 블로그';
+
+// 구글 서치 콘솔 'HTML 태그' 인증의 content 값만 넣는다. 비워두면 메타태그가 출력되지 않는다.
+export const GSC_VERIFICATION = '4gLotamjr3uRUsQ1iatVZKGbhLbEUm84Wc9Q1tCDMNs';
